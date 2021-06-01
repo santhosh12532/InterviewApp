edc9c793a990578f289dbbaa68825479c48e3583
santhosh12532 <santhosh12532@gmail.com>
2021-06-01 16:47:32 +0530

    commit on master

--- interviewapp/src/Components/InterviewQuestion/index.js
+++ interviewapp/src/Components/InterviewQuestion/index.js
@@ -2,54 +2,101 @@ import {Component} from 'react'
 
 import './index.css'
 
+let scoreCount = 0
+
 class InterviewQuestion extends Component {
 
-    updateOptionClass = () => {
-        return 'correct-ans'
+    renderScoreCard = () => {
+        return <div className="score-card">
+            <h1>TIME IS UP</h1>
+            <h1 className='heading'>Your Score : {scoreCount}</h1>
+        </div>
     }
-    
-    renderOptions = (correct_answer,option1,option2,option3) => {
+
+    renderClickedValue = (event) => {
         
+        const {resetTimeAndProceedToNextQuestion,isClicked} = this.props
+        
+        const {fetchedDataWithTimer=""} = this.props;
+        const {correct_answer}=fetchedDataWithTimer
+ 
+        let clickedValue = event.target.textContent;
+        console.log("event object",event.target)
+        
+        if(clickedValue===correct_answer){
+            scoreCount+=1
+            console.log(scoreCount)
+            
+            return resetTimeAndProceedToNextQuestion(event)
+        }else{
+            console.log("Wrong Answer")
+        }
+    }
+
+    renderOptions = (correct_answer,option1,option2,option3) => {
+        const {isClicked} = this.props
+        const correctOptionClass = isClicked ? "correct-ans":"option"
+        console.log("Render Options Function Called")
+        console.log(correctOptionClass)
         if(option2===""){
             return <div>
-                <li className="option">{option1}</li>
-                <li className="option">{correct_answer}</li>
+                <li className="option" onClick={this.renderClickedValue} value={option1}>{option1}</li>
+                <li className="option" onClick={this.renderClickedValue} value={correct_answer}>{correct_answer}</li>
 
             </div>
         }
         return <div>
-            <li className= "option" onClick={this.updateOptionClass}>{correct_answer}</li>
-            <li className="option" onClick={this.updateOptionClass}>{option1}</li>
-            <li className="option" onClick={this.updateOptionClass}>{option2}</li>
-            <li className="option" onClick={this.updateOptionClass}>{option3}</li>
+            
+            <li className= {`${correctOptionClass}`}value={correct_answer} onClick={this.renderClickedValue} >{correct_answer}</li>
+            <li className="option" value={option1} onClick={this.renderClickedValue} >{option1}</li>
+            <li className="option" value={option2} onClick={this.renderClickedValue} >{option2}</li>
+            <li className="option"  value={option3}onClick={this.renderClickedValue} >{option3}</li>
         </div>
     }
     
-    render(){
+
+
+    renderQuestion = () => {
         const {fetchedDataWithTimer=""} = this.props;
         const {questionNumber,timer} = this.props
-        const { question="Click on Start Quiz Button to start the quiz", correct_answer="correcti ans",option1="",option2="",option3=""} = fetchedDataWithTimer
-       console.log(" Question is ",question)
+        
+        const { question="Click on Start Quiz Button to start the quiz", correct_answer="correct ans",option1="",option2="",option3=""} = fetchedDataWithTimer
+
+        return <div >
+        <div className="heading-container">
+            <div className="score-container">
+                <p className="score">SCORE</p>
+                <p className="count">{scoreCount}/10</p>
+            </div>
+            <h1 className="heading">Question {questionNumber+1} of 10</h1>
+            <div className="time-container">
+                <p className="score">TIME</p>
+                <p className="count">{timer} sec(s)</p>
+            </div>
+        </div>
+        
+        
+        <p className="question">{questionNumber+1}.{question}</p>
+        <ul className="option-container" >
+            {this.renderOptions(correct_answer,option1,option2,option3)}
+        </ul>
+    </div>
+    }
+    
+    
+    render(){
+        // const {fetchedDataWithTimer=""} = this.props;
+         const {questionNumber} = this.props
+        // const { question="Click on Start Quiz Button to start the quiz", correct_answer="correcti ans",option1="",option2="",option3=""} = fetchedDataWithTimer
+       
             return(
-                    <div className="question-container">
-                        <div className="heading-container">
-                            <div className="score-container">
-                                <p className="score">SCORE</p>
-                                <p className="count">0/10</p>
-                            </div>
-                            <h1 className="heading">Question {questionNumber+1} of 10</h1>
-                            <div className="time-container">
-                                <p className="score">TIME</p>
-                                <p className="count">{timer} sec(s)</p>
-                            </div>
-                        </div>
-                        
-                        <p className="question">{question}</p>
-                        <ul className="option-container">
-                            {this.renderOptions(correct_answer,option1,option2,option3)}
-                        </ul>
-                    </div>
-        )
+                <div className="question-container">
+                    {
+                    questionNumber===10 ? this.renderScoreCard() : this.renderQuestion()
+                }
+                </div>
+                
+            )
     }
 }
 
--- interviewapp/src/Components/InterviewQuestionsApp/index.js
+++ interviewapp/src/Components/InterviewQuestionsApp/index.js
@@ -12,7 +12,8 @@ class InterviewQuestionsApp extends Component{
         questionNumber:0,
         timer:0,
         isQuizStarted:false,
-        isQuizEnd:true,
+        isClicked:false
+        
     }
 
     componentWillUnmount() {
@@ -25,19 +26,29 @@ class InterviewQuestionsApp extends Component{
         })
     }
 
+    resetTimeAndProceedToNextQuestion = value =>{
+        //clearInterval(this.timeInterval)
+        this.setState({
+            timer:9,
+            isClicked:true,
+        })
+    }
+
     updateTimer = () => {
         const {timer,questionNumber}= this.state
         if(timer>=10){
+            
             this.setState(prevState =>({
-                
                 timer:0,
                 questionNumber:prevState.questionNumber+1,
+                
             }))
         }
         else{
             this.setState(prevState =>({
                 isQuizStarted:true,
                 timer:prevState.timer+1,
+                isClicked:false,
             }))
         }
         if(questionNumber===10)
@@ -53,6 +64,7 @@ class InterviewQuestionsApp extends Component{
 
     fetchData = async () => {
         const {activeDifficultyLevel}= this.state
+        
         const response = await fetch(`https://opentdb.com/api.php?amount=10&category=18&difficulty=${activeDifficultyLevel}`)
         const data = await response.json()
         const fetchedData = data.results
@@ -72,12 +84,13 @@ class InterviewQuestionsApp extends Component{
     }
     render(){
         const {levelsData}=this.props
+        
          //const questionDataFromAPI =  this.fetchData();
-        const {fetchedQuestionData,questionNumber,timer,isQuizStarted} = this.state
+        const {fetchedQuestionData,questionNumber,timer,isQuizStarted,isClicked} = this.state
         
         const fetchedDataWithTimer = fetchedQuestionData[questionNumber]
 
-        console.log("with timer",fetchedDataWithTimer)
+        //console.log("with timer",fetchedDataWithTimer)
         return(
             <div className="app-container">
                 <h1 className="heading">
@@ -87,6 +100,8 @@ class InterviewQuestionsApp extends Component{
                 {isQuizStarted ? <InterviewQuestion fetchedDataWithTimer={fetchedDataWithTimer} 
                 questionNumber={questionNumber}
                 timer={timer}
+                resetTimeAndProceedToNextQuestion={this.resetTimeAndProceedToNextQuestion}
+                isClicked ={isClicked}
                 /> : "Click the below button to start Quiz"}
                 <div className="button-container">
                 <button className="start-button" onClick={this.fetchData}>Start Quiz</button>
